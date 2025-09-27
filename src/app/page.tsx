372e504ba1727310d31c3de41c3c80dc8e212130
Depo-Lonedev <oluwadamilare_daniel@outlook.com>
2025-09-27 12:46:58 +0100

    page reverted

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,9 @@
-import ThemeToggle from "@/components/themeToggler";
-import Image from "next/image";
+import BusinessDetails from "../components/business-details"
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center p-24 bg-[var(--color-background)] text-[var(--color-foreground)]">
-      <ThemeToggle />
-      <footer className="flex h-24 w-full items-center justify-center border-t mt-8">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    <div className="flex min-h-screen">
+      <BusinessDetails />
     </div>
   );
 }
