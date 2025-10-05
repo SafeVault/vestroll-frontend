f38f72379b9686b1b65059cc5281f011d0d2c2ba
Ridwan Abiola <ricklick262@gmail.com>
2025-10-05 17:51:43 +0100

    feat: add steps - contract details, employee details and more

--- src/app/(app)/contracts/page.tsx
+++ src/app/(app)/contracts/page.tsx
@@ -3,9 +3,9 @@ import Contracts from "@/components/contracts";
 
 export default function ContractsPage() {
   return (
-    <main className=" space-y-4">
+    <div className=" space-y-4">
       <PageHeader />
       <Contracts />
-    </main>
+    </div>
   );
 }
