9fd0cc28f83144631e45984d4a41dc109f1f5684
Clement Raymond <chinexzy37@gmail.com>
2025-10-05 17:16:47 +0000

    Removed mock routing from page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
-// import VestRollAccountSelection from '@/components/accountType';
-import TimesheetDetails from '@/components/dashboard/timesheet/TimesheetDetails';
+import VestRollAccountSelection from '@/components/accountType';
 import { redirect } from 'next/navigation';
 
 export default function Home() {
   return (
     <div>
-      <TimesheetDetails />
+      <VestRollAccountSelection />
       {/* redirect("/onboarding"); */}
     </div>
   );
